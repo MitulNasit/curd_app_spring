0ee0dc912d26e722dd20fe6cf0547a706332ea94
MitulNasit <mitunasit000@gmail.com>
2022-12-08 20:31:45 +0100

    first upload

---
+++ src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -0,0 +1,29 @@
+package net.javaguides.springboot.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.javaguides.springboot.model.Employee;
+import net.javaguides.springboot.repository.EmployeeRepository;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	
+	//get all employess 
+	
+	@GetMapping("/employees")
+	public List<Employee> getAllEmployess(){
+		return employeeRepository.findAll();
+		
+	}
+	
+
+}
