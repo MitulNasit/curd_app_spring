ec9e4a1fdc142bd62e055a6539091bc5c44f1b0e
MitulNasit <mitunasit000@gmail.com>
2022-12-14 22:27:13 +0100

    spring curd operation

--- src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -3,27 +3,71 @@ package net.javaguides.springboot.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import net.javaguides.springboot.exception.ResourceNotFoundException;
 import net.javaguides.springboot.model.Employee;
 import net.javaguides.springboot.repository.EmployeeRepository;
 
 @RestController
 @RequestMapping("/api/v1/")
+@CrossOrigin("*")
+
 public class EmployeeController {
 	@Autowired
 	private EmployeeRepository employeeRepository;
 	
 	
 	//get all employess 
-	
+	@CrossOrigin
 	@GetMapping("/employees")
 	public List<Employee> getAllEmployess(){
 		return employeeRepository.findAll();
 		
 	}
 	
-
+	
+	//create employee rest api
+	@PostMapping("/employees")
+	public Employee createEmployee(@RequestBody Employee employee)
+	{
+		return employeeRepository.save(employee);
+	}
+	
+	//get employee by id rest api
+	
+	@GetMapping("/employees/{id}")
+	public ResponseEntity<Employee> getEmployeeByID(@PathVariable Long id) {
+		
+		Employee employee = employeeRepository.findById(id)
+				.orElseThrow(()-> new ResourceNotFoundException("Employee not exist bwith ID:" + id));
+		
+		return ResponseEntity.ok(employee);
+	
+	}
+	
+	//update employee rest api
+	@PutMapping("/employees/{id}")
+	public ResponseEntity<Employee> updateEmployee(@PathVariable Long id,@RequestBody Employee employeeDetails){
+		
+		Employee employee = employeeRepository.findById(id)
+				.orElseThrow(()-> new ResourceNotFoundException("Employee not exist bwith ID:" + id));
+		
+		employee.setFirstname(employeeDetails.getFirstname());
+		employee.setLastname(employeeDetails.getLastname());
+		employee.setEmailID(employeeDetails.getEmailID());
+		
+		Employee updatedEmployee= employeeRepository.save(employee);
+		return ResponseEntity.ok(updatedEmployee);
+		
+	}
+	
 }
--- src/main/java/net/javaguides/springboot/exception/ResourceNotFoundException.java
+++ src/main/java/net/javaguides/springboot/exception/ResourceNotFoundException.java
@@ -12,6 +12,7 @@ public class ResourceNotFoundException extends RuntimeException{
 	public ResourceNotFoundException(String message) {
 		super(message);
 	}
-
+	
 
 }
+
